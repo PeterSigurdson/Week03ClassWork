bfdfa8778311d7f374cb637d0613e104a8624f53
Peter Sigurdson <peter@petersigurdson.net>
2019-05-21 14:39:47 -0400

    part B

---
+++ C099999/C099999/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C099999
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a number");
+            string num1 = Console.ReadLine();
+
+            Console.WriteLine("please enter the 2nd number");
+            string num2 = Console.ReadLine();
+
+            int sum = Convert.ToInt32(num1) + Convert.ToInt32(num2);
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
